b69342af2b8f867627c5f8d29843519cf17f0e56
Christopher Belanger <christopher@belangeranalytics.com>
2022-11-18 15:13:45 -0500

    Add gen2 and gen3 shapefiles

---
+++ R/data.R
@@ -0,0 +1,5 @@
+#' Ottawa Neighbourhood Study Spatial Boundaries (Gen2)
+"ons_shp_gen2"
+
+#' Ottawa Neighbourhood Study Spatial Boundaries (Gen3)
+"ons_shp_gen3"
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,11 @@
-## code to prepare `DATASET` dataset goes here
+## code to prepare data goes here
 
-usethis::use_data(DATASET, overwrite = TRUE)
+
+ons_shp_gen2 <- sf::read_sf("data-raw/ons_shp_gen2/ons_shp_gen2.shp")
+ons_shp_gen2 <- sf::st_transform(ons_shp_gen2, crs = "WGS84")
+
+usethis::use_data(ons_shp_gen2, overwrite = TRUE)
+
+ons_shp_gen3 <- sf::read_sf("data-raw/ons_shp_gen3/Final_Gen3_Sep2022.shp")
+ons_shp_gen3 <- sf::st_transform(ons_shp_gen3, crs = "WGS84")
+usethis::use_data(ons_shp_gen3, overwrite = TRUE)
