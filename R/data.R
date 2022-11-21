1e5bc506a3f2486b43794021779d890cb532874c
Christopher Belanger <christopher@belangeranalytics.com>
2022-11-21 09:49:47 -0500

    Documentation edit for ons_shp files

--- R/data.R
+++ R/data.R
@@ -1,5 +1,20 @@
 #' Ottawa Neighbourhood Study Spatial Boundaries (Gen2)
+#'
+#' The ONS Neighbourhood Boundaries were created by the Ottawa Neighbourhood Study
+#' (ONS) to analyse population statistics and are not indicative of actual neighbourhood limits.
+#'
+#' Available publicly from the City of Ottawa: \url{https://open.ottawa.ca/datasets/32fe76b71c5e424fab19fec1f180ec18_0/explore}
+#'
+#' @family shapefiles
+#' @seealso ons_shp_gen3
+#'
 "ons_shp_gen2"
 
 #' Ottawa Neighbourhood Study Spatial Boundaries (Gen3)
+#'
+#' The ONS Neighbourhood Boundaries were created by the Ottawa Neighbourhood Study
+#' (ONS) to analyse population statistics and are not indicative of actual neighbourhood limits.
+#'
+#' @family shapefiles
+#' @seealso ons_shp_gen2
 "ons_shp_gen3"
